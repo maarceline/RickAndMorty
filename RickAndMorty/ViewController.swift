51275edfea9eac0615bade25ae2ab764cb1526ff
Marce <maarceline00@gmail.com>
2025-05-06 12:58:03 +0200

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Marcela Hernández on 6/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RickAndMortyTests/RickAndMortyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTests.swift
+//  RickAndMortyTests
+//
+//  Created by Marcela Hernández on 6/5/25.
+//
+
+import Testing
+@testable import RickAndMorty
+
+struct RickAndMortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
